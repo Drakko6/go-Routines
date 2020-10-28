50376900aa037e4e2084d6553d19b42c576bc082
César Tonatiuh Tamayo Ávila <cesar.tamayo@alumnos.udg.mx>
2020-10-28 04:11:15 -0600

    Funciona agregar, mostrar y terminar procesos

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"./procesos"
+	"os"
+	"os/exec"
+	
+)
+
+
+func main () {
+
+
+	procesosSlice := procesos.Procesos{Procesos: []*procesos.Proceso{}}
+	id:= uint64(0)
+
+
+	var op uint = 1
+
+	imprimiendo := 0
+	cont :=0
+
+	for op != 0 {
+		cmd := exec.Command("cmd", "/c", "cls") 
+        cmd.Stdout = os.Stdout
+        cmd.Run()
+		fmt.Println("")
+		fmt.Println("	ADMINISTRADOR DE PROCESOS")
+		fmt.Println("1.- Agregar proceso")
+		fmt.Println("2.- Mostrar procesos")
+		fmt.Println("3.- Terminar proceso")
+		fmt.Println("0.- Salir")
+
+		fmt.Scan(&op)
+
+		switch {
+		case op == 1:
+			
+			p := procesos.Proceso{ID: id}
+			procesosSlice.Procesos = append(procesosSlice.Procesos, &p)
+			go p.Start()
+			id++
+
+		case op == 2:
+
+			if(imprimiendo == 0){
+				cont = 0
+			}
+			imprimiendo = 1
+
+			if cont != 0  {
+				imprimiendo = 0 
+			}
+
+			if(imprimiendo==1){
+			procesosSlice.Mostrar(&imprimiendo)
+			}
+
+			cont++
+
+
+		case op == 3:
+
+			var numProceso uint64
+
+			fmt.Println("ID de proceso a terminar: ")
+			fmt.Scan(&numProceso)
+
+
+			i:= procesosSlice.Buscar(numProceso)
+			procesosSlice.Procesos = procesosSlice.Borrar(i)
+			
+		
+		case op == 0:
+			fmt.Println("Saliendo...")
+
+		default:
+			fmt.Println("Opción inválida")
+		}
+
+	}
+
+
+}
---
+++ procesos/procesos.go
@@ -0,0 +1,71 @@
+package procesos
+
+import(
+	"fmt"
+	"time"
+)
+
+type Procesos struct {
+	Procesos []*Proceso
+}
+
+func (p *Procesos) Mostrar(imprimiendo *int) {
+
+	for _, p := range p.Procesos{
+		
+		go p.Mostrar(imprimiendo)
+		if *imprimiendo == 0 {
+			break
+		}
+	}
+}
+
+func (p* Procesos) Buscar (ID uint64) int{ 
+
+	var indice int
+	for i:=0; i<len(p.Procesos); i++{
+		if p.Procesos[i].ID == ID {
+			indice = i
+			return indice
+		}
+	}
+	return -1
+
+}
+
+func (p *Procesos)Borrar( i int) []*Proceso {
+    return append(p.Procesos[:i], p.Procesos[i+1:]...)
+}
+
+
+type Proceso struct {
+	ID uint64 
+	i uint64 
+
+}
+
+
+func (p *Proceso) Start() {
+	 
+		for {
+			p.i = p.i + 1
+			time.Sleep(time.Millisecond * 500)
+		}
+	
+}
+
+func (p * Proceso) Mostrar(imprimiendo *int) {
+
+		for{
+			fmt.Printf("id %d: %d", p.ID, p.i)
+			fmt.Println("")
+			time.Sleep(time.Millisecond * 500)
+			if *imprimiendo == 0 {
+				break
+			}
+		}
+	
+}
+
+
+
